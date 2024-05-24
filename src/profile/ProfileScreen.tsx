b8b641a0df8b816b250150fee6a54fe72d0628cc
salihgueler <muhammedsalihguler@gmail.com>
2024-05-24 21:43:49 +0200

    Update the profile and package.json

--- src/profile/ProfileScreen.tsx
+++ src/profile/ProfileScreen.tsx
@@ -1,7 +1,4 @@
 import React, { useState, useEffect } from "react";
-import * as TaskManager from "expo-task-manager";
-import * as BackgroundFetch from "expo-background-fetch";
-const UPLOAD_TASK_NAME = "FILE_UPLOAD_TASK";
 
 import {
   View,
